5f1bb6d387c9910a99cc176820ab5bd9d8ca25a2
Yacine BOUKARI <steeveboukari9@gmail.com>
2025-12-17 12:39:15 +0100

    feat: add files

--- exercise2/part1_all.R
+++ exercise2/part1_all.R
@@ -1,4 +1,27 @@
+library(dplyr)
+
+DIRECTORY = "prenoms-par-dpts"
+
+filenames = list.files(DIRECTORY, pattern="*.csv")
+
+all_data = lapply(filenames, function(filename) read.csv(paste(DIRECTORY, filename, sep="/"), sep=";"))
+all_data_2 = lapply(all_data, function(data) mutate(data, PrenomAnnee=paste0(preusuel, "_", annais)))
+
+head(all_data_2[[1]])
+
 data = read.csv("prenomsRennesStrassNantesToul.csv", sep=";")
 head(data)
 
-data %>% group_by(ANNAISS)
+sort(unique(data$ANNAISS))
+
+unique(data$LBCOM)
+
+data %>% group_by(LBCOM)
+
+cos_sim = function(a, b) {
+  sum(a * b) / sqrt(sum(a**2) * sum(b**2))
+}
+
+data_by_city = split(data, data$LBCOM)
+
+data_by_city$NANTES$Prn
